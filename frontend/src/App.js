58033a62334c679c4afc0ab6c87572115fd18dd5
Archit Kandu <architkandu@hotmail.com>
2023-09-14 02:55:46 +0530

    Frontend Environment

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Button} from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
