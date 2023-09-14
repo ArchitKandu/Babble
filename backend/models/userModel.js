1a0f86c65b8704ec1ec9e656826dbadd1e317ce3
Archit Kandu <architkandu@hotmail.com>
2023-09-14 18:23:44 +0530

    Database Schemas

---
+++ backend/models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const userModel = mongoose.Schema({
+    name:{ type: String, required: true },
+    email:{ type: String, required: true },
+    password:{ type: String, required: true },
+    pic:{
+        type: String,
+        required: true,
+        default: 'https://freesvg.org/img/abstract-user-flat-4.png'
+    }
+},{ timestamps: true });
+const User = mongoose.model('User',userModel);
+module.exports = User;
