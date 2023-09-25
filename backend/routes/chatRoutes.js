45a831cbca70941e27c99d693314379f2de8a629
Archit Kandu <architkandu@hotmail.com>
2023-09-25 23:33:24 +0530

    TypeError

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const { accessChat } = require('../controllers/chatControllers');
-const { notFound, errorHandler } = require('../middleware/errorMiddleware');
 
 const router = express.Router();
-router.use(notFound);
-router.use(errorHandler);
 
 router.use('/').post(protect, accessChat);
 // router.use('/').get(protect, fetchChats);
