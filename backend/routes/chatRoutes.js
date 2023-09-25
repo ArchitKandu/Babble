327896201e1d8c7b7deb8a85b6c36999a09860f0
Archit Kandu <architkandu@hotmail.com>
2023-09-26 00:24:24 +0530

    Fetch All Chats

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { accessChat } = require('../controllers/chatControllers');
+const { accessChat, fetchChats } = require('../controllers/chatControllers');
 
 const router = express.Router();
 
 router.post('/', protect, accessChat);
-// router.get('/', protect, fetchChats);
+router.get('/', protect, fetchChats);
 // router.post('/group', protect, createGroupChat);
 // router.put('/rename', protect, renameGroup);
 // router.put('/groupremove', protect, removeFromGroup);
