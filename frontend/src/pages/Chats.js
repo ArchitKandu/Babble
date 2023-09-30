a35655f3ac91c49efef49e2c5922c9836d5f21e2
Archit Kandu <architkandu@hotmail.com>
2023-09-30 21:25:36 +0530

    Frontend Started

--- frontend/src/pages/Chats.js
+++ frontend/src/pages/Chats.js
@@ -1,6 +1,8 @@
 import { Box } from "@chakra-ui/react";
 import { useChatState } from "../context/chatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChats from "../components/miscellaneous/MyChats";
+import ChatBox from "../components/miscellaneous/ChatBox";
 
 const Chats = () => {
     const { user } = useChatState();
@@ -8,9 +10,15 @@ const Chats = () => {
         <div style={{ width: "100%" }}>
             { user && <SideDrawer/> }
             {/* <SideDrawer/> */}
-            <Box>
-                {/* { user && <MyChats/> } */}
-                {/* { user && <ChatBox/> } */}
+            <Box
+                display = 'flex'
+                justifyContent='space-between'
+                width='100%'
+                height='91.5vh'
+                padding='10px'
+            >
+                { user && <MyChats/> }
+                { user && <ChatBox/> }
             </Box>
         </div>
     );
