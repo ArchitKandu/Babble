8fa48b2911655fe053d2b052127755561d549e22
Archit Kandu <architkandu@hotmail.com>
2023-10-01 21:34:45 +0530

    Top Doing

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,8 @@
-import { Box, Button, Text, Tooltip } from '@chakra-ui/react';
+import { Box, Button, Menu, MenuButton, MenuList, Text, Tooltip } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { BellIcon } from '@chakra-ui/icons';
 
 const SideDrawer = () => {
   const [ search, setSearch ] = useState('');
@@ -10,13 +11,33 @@ const SideDrawer = () => {
   const [ loadingChat, setLoadingChat ] = useState(false);
   return (
     <>
-     <Box>
+     <Box
+      display='flex'
+      justifyContent='space-between'
+      alignItems='center'
+      bg='white'
+      w='100%'
+      p='5px 10px 5px 10px'
+      borderWidth='5px'
+     >
         <Tooltip label='Search Users to Chat ' hasArrow placement='bottom-end'>
           <Button variant='ghost'>
             <FontAwesomeIcon icon={faMagnifyingGlass}/>
-            <Text display={{base:'none', md:'flex'}}>Search User</Text>
+            <Text display={{base:'none', md:'flex'}} px='4'>Search User</Text>
           </Button>
         </Tooltip>
+        <Text
+          fontSize='2xl'
+          fontFamily='monospace'
+        >BABBLE</Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize='2xl' m={1}/>
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+        </div>
       </Box> 
     </>
   );
