424542240622908afde43d738e1fdbf4d07e81a7
Archit Kandu <architkandu@hotmail.com>
2023-10-02 19:44:01 +0530

    Drawer Started

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,70 +1,103 @@
-import { Avatar, Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons';
-import { useChatState } from '../../context/chatProvider';
-import ProfileModals from './ProfileModals';
-import { useHistory } from 'react-router-dom';
+import {
+  Avatar,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { useChatState } from "../../context/chatProvider";
+import ProfileModals from "./ProfileModals";
+import { useHistory } from "react-router-dom";
 
 const SideDrawer = () => {
-  const [ search, setSearch ] = useState('');
-  const [ searchResult, setSearchResult ] = useState([]);
-  const [ loading, setLoading ] = useState(false);
-  const [ loadingChat, setLoadingChat ] = useState(false);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
 
   const { user } = useChatState();
   const history = useHistory();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const logoutHandler = () => {
-    localStorage.removeItem('userInfo');
-    history.push('/');
-  }
+    localStorage.removeItem("userInfo");
+    history.push("/");
+  };
 
   return (
     <>
-     <Box
-      display='flex'
-      justifyContent='space-between'
-      alignItems='center'
-      bg='white'
-      w='100%'
-      p='5px 10px 5px 10px'
-      borderWidth='5px'
-     >
-        <Tooltip label='Search Users to Chat ' hasArrow placement='bottom-end'>
-          <Button variant='ghost'>
-            <FontAwesomeIcon icon={faMagnifyingGlass}/>
-            <Text display={{base:'none', md:'flex'}} px='4'>Search User</Text>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users to Chat " hasArrow placement="bottom-end">
+          <Button variant="ghost" onClick={onOpen}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <Text display={{ base: "none", md: "flex" }} px="4">
+              Search User
+            </Text>
           </Button>
         </Tooltip>
-        <Text
-          fontSize='2xl'
-          fontFamily='monospace'
-        >BABBLE</Text>
+        <Text fontSize="2xl" fontFamily="monospace">
+          BABBLE
+        </Text>
         <div>
           <Menu>
             <MenuButton p={1}>
-              <BellIcon fontSize='2xl' m={1}/>
+              <BellIcon fontSize="2xl" m={1} />
             </MenuButton>
             {/* <MenuList></MenuList> */}
             <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon/>}>
-                  <Avatar size='sm' cursor='pointer' name={user.name} src={user.pic}/>
+              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                <Avatar
+                  size="sm"
+                  cursor="pointer"
+                  name={user.name}
+                  src={user.pic}
+                />
               </MenuButton>
               <MenuList>
                 <ProfileModals user={user}>
                   <MenuItem>My Profile</MenuItem>
                 </ProfileModals>
-                <MenuDivider/>
+                <MenuDivider />
                 <MenuItem onClick={logoutHandler}>Logout</MenuItem>
               </MenuList>
             </Menu>
           </Menu>
         </div>
-      </Box> 
+      </Box>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search User</DrawerHeader>
+        </DrawerContent>
+        <DrawerBody>
+          <Box d="flex" pb={2}></Box>
+        </DrawerBody>
+      </Drawer>
     </>
   );
-}
+};
 
 export default SideDrawer;
