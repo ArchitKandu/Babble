90245dcde11f10b73d99e4d198435d175ddc0a21
Archit Kandu <architkandu@hotmail.com>
2023-10-29 15:30:43 +0530

    Serverside Setup Socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express=require("express");
-const dotenv=require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 app.use(express.json());
@@ -12,14 +12,25 @@ app.use(express.json());
 dotenv.config();
 connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("API is Called!!")
+app.get("/", (req, res) => {
+  res.send("API is Called!!");
 }); //SIMPLE API CALLED
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(5000,console.log("Server started on PORT 5000!!"));
+const server = app.listen(5000, console.log("Server started on PORT 5000!!"));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to Socket.io !!");
+});
